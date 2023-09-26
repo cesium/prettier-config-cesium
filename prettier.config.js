bb6f4b058fa3627ddc156a3fcdcb23fc575ab8f6
Enzo Vieira <enzovieira16@hotmail.com>
2023-09-26 22:04:54 +0100

    fix: resolve config file conflicts
    feat: add prettier-plugin-tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  tailwindFunctions: ["clsx", "twMerge", "cn", "classNames"],
+};
